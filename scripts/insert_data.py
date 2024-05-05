6b626b32e8cedac6e3b6cf50756a38d458a0b0aa
20km-shimakze <2801382156@qq.com>
2024-05-05 12:04:24 +0000

    数据插入脚本

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -11,12 +11,14 @@ from sentence_transformers import SentenceTransformer
 
 
 def process_sentence(model: SentenceTransformer, sentences: list)-> list:
-    logging.info("processing embedding start")
     sentence_list = []
     for sentence in tqdm(sentences, desc="process sentence"):
         encode_sentence = model.encode(sentence)
-        sentence_list.append(encode_sentence)
-    logging.info("processing embedding finish")
+        sentence_list.append({
+            "vector": encode_sentence,
+            "law_type": "law_book",
+            "info": sentence
+        })
     return sentence_list
 
 
@@ -25,12 +27,12 @@ utils.configure_logging()
 
 logging.info("load model start")
 model_path = "chestnutlzj/ChatLaw-Text2Vec"
-model = SentenceTransformer(model_path, cache_folder="../models").cuda()
+model = SentenceTransformer(model_path, cache_folder="./models").cuda()
 logging.info("load model finish")
 
 
 logging.info("load law_data start")
-law_book_path = ""
+law_book_path = "./law_data/law_book"
 law_book_dataset = utils.load_datasets(law_book_path)['train']
 law_books = []
 data_list = len(law_book_dataset)
@@ -39,8 +41,20 @@ for i in tqdm(law_book_dataset, desc="processing str"):
 ans = utils.splice_prompt(law_books, "law_book")
 logging.info("load law_data finish")
 
-
 # processing sentence
-process_sentence(model, law_books)
+logging.info("processing embedding start")
+insert_datas = process_sentence(model, ans)
+logging.info("processing embedding finish")
+
+
+logging.info("insert colle strat")
+client = utils.get_client()
+db.load_colle(client, "law_vec")
+for insert_data in tqdm(insert_datas, desc="process insert"):
+    if len(insert_data["info"]) >= 10000:
+        logging.warning(f"out len 51200, len is {len(insert_data['info'])}")
+        continue
+    db.insert_data(client, [insert_data], "law_vec")
+logging.info("insert colle finish")
 
 
